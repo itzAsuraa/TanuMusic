01fd15c9c2c17bfcbe8ee769a4b4573fb8a15481
⏤͟͟͞͞ 🇮🇳 𝐓ʜᴇ 𝐂ᴀᴘᴛᴀɪɴ's / <167273277+itzAsuraa@users.noreply.github.com>
2024-10-09 19:53:42 +0530

    Update resume.py

--- TanuMusic/plugins/admins/resume.py
+++ TanuMusic/plugins/admins/resume.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message
 from config import BANNED_USERS
 from strings import get_command
 from TanuMusic import app
-from TanuMusic.core.call import Dns
+from TanuMusic.core.call import Tanu 
 from TanuMusic.utils.database import is_music_playing, music_on
 from TanuMusic.utils.decorators import AdminRightsCheck
 
@@ -20,5 +20,5 @@ async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
-    await Dns.resume_stream(chat_id)
+    await Tanu.resume_stream(chat_id)
     await message.reply_text(_["admin_4"].format(message.from_user.mention))
