e8e9e8ea52b8be31e3fca069162388c74c22613e
⏤͟͟͞͞ 🇮🇳 𝐓ʜᴇ 𝐂ᴀᴘᴛᴀɪɴ's / <167273277+itzAsuraa@users.noreply.github.com>
2024-10-09 19:45:18 +0530

    Update bot.py

--- TanuMusic/core/bot.py
+++ TanuMusic/core/bot.py
@@ -19,7 +19,7 @@ import config
 from ..logging import LOGGER
 
 
-class DnsBot(Client):
+class TanuBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
