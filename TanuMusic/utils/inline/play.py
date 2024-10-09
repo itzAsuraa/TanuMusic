1c83ead38899ee992dfe4e33188b6f6114b2312a
⏤͟͟͞͞ 🇮🇳 𝐓ʜᴇ 𝐂ᴀᴘᴛᴀɪɴ's / <167273277+itzAsuraa@users.noreply.github.com>
2024-10-09 20:03:32 +0530

    Update play.py

--- TanuMusic/utils/inline/play.py
+++ TanuMusic/utils/inline/play.py
@@ -170,11 +170,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"DnsPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"Playlists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"DnsPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"Playlists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
