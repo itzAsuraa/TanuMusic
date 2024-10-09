7f727e0c3c6343735f62adc1bd439bf223962f57
⏤͟͟͞͞ 🇮🇳 𝐓ʜᴇ 𝐂ᴀᴘᴛᴀɪɴ's / <167273277+itzAsuraa@users.noreply.github.com>
2024-10-09 10:34:20 +0530

    Update thumbnails.py

--- TanuMusic/utils/thumbnails.py
+++ TanuMusic/utils/thumbnails.py
@@ -8,6 +8,16 @@ from youtubesearchpython.__future__ import VideosSearch
 from TanuMusic import app
 from config import YOUTUBE_IMG_URL
 
+async def gen_thumb(videoid):
+    try:
+        query = f"https://www.youtube.com/watch?v={videoid}"
+        results = VideosSearch(query, limit=1)
+        for result in (await results.next())["result"]:
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+        return thumbnail
+    except Exception as e:
+        return YOUTUBE_IMG_URL
+
 def changeImageSize(maxWidth, maxHeight, image):
     widthRatio = maxWidth / image.size[0]
     heightRatio = maxHeight / image.size[1]
