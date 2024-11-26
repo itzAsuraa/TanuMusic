2f5da69e71a5818cb42f53841e371ddd9990ffe5
⏤͟͟͞͞ 🇮🇳 𝐓ʜᴇ 𝐂ᴀᴘᴛᴀɪɴ's / <167273277+itzAsuraa@users.noreply.github.com>
2024-11-26 19:11:31 +0530

    Update

--- config/config.py
+++ config/config.py
@@ -53,6 +53,12 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  #❖ Fill this variable if your upstream repository is private
 
+#❖ Make your bots privacy from telegra.ph and put your url here
+PRIVACY_LINK = getenv(
+    "PRIVACY_LINK", "https://graph.org/Privacy-Policy-for-Tanu-Music-11-26"
+)
+
+
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/C0DE_SEARCH")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AsuraaSupports")
 
